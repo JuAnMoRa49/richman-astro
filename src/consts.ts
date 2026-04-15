55dcd51012f7a74cd36d9836a5f9fd4c143e8e73
JuAnMoRa49 <125929793+JuAnMoRa49@users.noreply.github.com>
2026-04-15 09:06:12 -0600

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Richman Componentes";
+export const SITE_DESCRIPTION =
+  "Empresa dedicada a la venta de componentes para automotriz, ofreciendo productos de alta calidad y un servicio excepcional a nuestros clientes.";
